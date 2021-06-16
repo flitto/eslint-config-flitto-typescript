0780f77b32e44fa61773ce0e1d74ceada2267c74
chanwool <chanwool.park@flitto.com>
2021-06-16 18:21:20 +0900

    룰 상세 변경: comma-dangle

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: 'standard-with-typescript',
     rules: {
-        "comma-dangle": ["error", "never"],
+        'comma-dangle': ['error', 'always-multiline'],
     }
 }
