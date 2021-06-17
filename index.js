3eaaabd6b098ee5b5abf38e416e613827d1c437f
chanwool <chanwool.park@flitto.com>
2021-06-17 16:10:13 +0900

    룰 제거 & 추가
    * 추가
      * @typescript-eslint/naming-convention
      * camelcase
    * 제거
      * @typescript-eslint/strict-boolean-expressions (내부 검토 후 결정)

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 module.exports = {
     extends: 'standard-with-typescript',
     rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/strict-boolean-expressions': 'off',
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                'selector': 'interface',
+                'format': ['PascalCase'],
+                'custom': {
+                    'regex': '^I[A-Z]',
+                    'match': true
+                }
+            },
+            {
+                'selector': 'class',
+                'format': ['PascalCase'],
+            }
+        ],
+
         'comma-dangle': ['error', 'always-multiline'],
+        "camelcase": ["error", { "properties": "never", "ignoreDestructuring": true, "ignoreImports": true }],
     }
 }
