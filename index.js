0d11cfbadaee605c3f6c214fb8b490e8bd597556
chanwool <chanwool.park@flitto.com>
2024-07-08 14:14:30 +0900

    build: pkg version update

--- index.js
+++ index.js
@@ -1,62 +1,70 @@
-module.exports = {
-  extends: [
-    'love',
-    'prettier'
-  ],
-  plugins: ['prettier'],
-  rules: {
-    '@typescript-eslint/naming-convention': [
-      'error',
-      {
-        'selector': 'interface',
-        'format': ['StrictPascalCase'],
-      },
-      {
-        'selector': 'class',
-        'format': ['StrictPascalCase'],
-      },
-      {
-        'selector': ['property'],
-        'format': ['snake_case', 'strictCamelCase', 'StrictPascalCase', 'UPPER_CASE'],
-      },
-      {
-        'selector': ['property'],
-        'modifiers': ['requiresQuotes'],
-        'format': null,
-      },
-      {
-        'selector': ['property'],
-        'filter': {
-          'regex': '^(_id|__v)$',
-          'match': true,
+import eslintConfigLove from 'eslint-config-love'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettier from 'eslint-config-prettier'
+
+export default [
+  eslintConfigLove,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          'selector': 'interface',
+          'format': ['StrictPascalCase'],
         },
-        'format': null,
-      },
-      {
-        'selector': ['enum'],
-        'format': ['UPPER_CASE', 'StrictPascalCase'],
-      },
-      {
-        'selector': ['enumMember'],
-        'format': ['UPPER_CASE'],
-      },
-      {
-        'selector': 'variable',
-        'format': ['strictCamelCase', 'UPPER_CASE'],
-      },
-      {
-        'selector': 'variable',
-        'modifiers': ['destructured'],
-        'format': null,
-      },
-    ],
-    'prettier/prettier': ['error', {
-      'singleQuote': true,
-      'semi': false,
-      'tabWidth': 2,
-      'printWidth': 140,
-      'bracketSameLine': false,
-    }]
-  },
-  reportUnusedDisableDirectives: true,
-}
+        {
+          'selector': 'class',
+          'format': ['StrictPascalCase'],
+        },
+        {
+          'selector': ['property'],
+          'format': ['snake_case', 'strictCamelCase', 'StrictPascalCase', 'UPPER_CASE'],
+        },
+        {
+          'selector': ['property'],
+          'modifiers': ['requiresQuotes'],
+          'format': null,
+        },
+        {
+          'selector': ['property'],
+          'filter': {
+            'regex': '^(_id|__v)$',
+            'match': true,
+          },
+          'format': null,
+        },
+        {
+          'selector': ['enum'],
+          'format': ['UPPER_CASE', 'StrictPascalCase'],
+        },
+        {
+          'selector': ['enumMember'],
+          'format': ['UPPER_CASE'],
+        },
+        {
+          'selector': 'variable',
+          'format': ['strictCamelCase', 'UPPER_CASE'],
+        },
+        {
+          'selector': 'variable',
+          'modifiers': ['destructured'],
+          'format': null,
+        },
+      ],
+      'prettier/prettier': ['error', {
+        'singleQuote': true,
+        'semi': false,
+        'tabWidth': 2,
+        'printWidth': 140,
+        'bracketSameLine': false,
+      }]
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+  }
+]
